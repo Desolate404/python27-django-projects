682d86816def64eb7496060f535a28846702ec1f
Ascar <cherry400kg@gmail.com>
2023-04-18 11:53:06 +0600

    comments + permissins

--- blog/post/serializers.py
+++ blog/post/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Post
+from review.serializers import CommentSerializer
 
 
 class PostSerializer(ModelSerializer):
@@ -10,6 +11,8 @@ class PostSerializer(ModelSerializer):
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         rep['likes'] = instance.Likes.all().count()
+        comments = instance.comments.all()# все коменты
+        rep['comments'] = CommentSerializer(comments, many=True).data
         return rep
 
 
--- blog/review/models.py
+++ blog/review/models.py
@@ -4,3 +4,11 @@ from post.models import Post, User
 class Like(models.Model):
     user = models.ForeignKey(User, related_name='Likes', on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name='Likes', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
